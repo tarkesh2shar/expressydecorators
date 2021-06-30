63638fdff418902413dd63cd9fdcb527384ee848
tarkesh2shar <tarkesh2shar@gmail.com>
2021-06-30 20:23:30 +0530

    making swagger optional

--- src/example/example.ts
+++ src/example/example.ts
@@ -11,21 +11,26 @@ app.get("/", (req: Request, res: Response) => {
   res.send({ hello: "world" });
 });
 InitializeDecorators(router);
-InitializeSwagger({
-  swagger: "2.0",
-  basePath: "/",
-  definitions: {},
-  host: "localhost:8080",
-  info: {
-    version: "1.0.0",
-    title: "Basic swagger implementation",
-    description: "A sample API here",
+InitializeSwagger(
+  {
+    swagger: "2.0",
+    basePath: "/",
+    definitions: {},
+    host: "localhost:8080",
+    info: {
+      version: "1.0.0",
+      title: "Basic swagger implementation",
+      description: "A sample API here",
+    },
+    paths: {},
+    schemes: ["http", "https"],
   },
-  paths: {},
-  schemes: ["http", "https"],
-});
+  () => {
+    app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+  }
+);
 app.use(express.json());
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use(router);
 app.listen("8080", () => {
   console.log("listening to port 8008 !");
