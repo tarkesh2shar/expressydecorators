aaf45749b12d82e49f0434c90c934c658a64f1dd
tarkesh2shar <tarkesh2shar@gmail.com>
2021-06-30 09:13:29 +0530

    Added swagger decorator and Express Routes data fetcher decorator

--- src/example/SampleController.ts
+++ src/example/SampleController.ts
@@ -1,9 +1,101 @@
-import express,{Request,Response,NextFunction,Router} from 'express'
-import {Controller,Get} from '../'
-@Controller('/auth')
+import {
+  Controller,
+  Get,
+  Post,
+  Swaggerize,
+  getBody,
+  getParam,
+  getQuery,
+  Params,
+  Queries,
+  Body,
+  swaggerResponseCodes,
+  swaggerParamsIn,
+  swaggerParamsType,
+} from "../";
+//////////////////////////////////////////
+@Controller("/auth")
 export class LoginController {
-    @Get('/login') 
-    getLogin(req:Request,res:Response){
-        res.send({hello:"world"}) 
-    } 
+  @Get("/login/:accountId/:userId/salute/:soundId/awesome")
+  @Swaggerize({
+    tags: ["login"],
+    summary: "Login Routes",
+    description: "Just checking basic Routes Specs ok ",
+    responses: {
+      [swaggerResponseCodes.success]: {
+        description: "What are we getting back in the response allright ?",
+        content: "application/json",
+        schema: {
+          type: "object",
+          properties: {
+            Hello: {
+              type: "string",
+            },
+          },
+        },
+      },
+    },
+    parameters: [
+      {
+        description: "hi",
+        in: swaggerParamsIn.query,
+        name: "allGood",
+        type: swaggerParamsType.integer,
+        required: true,
+      },
+      {
+        name: "accountId",
+        in: swaggerParamsIn.path,
+        description: "This is used to fetch information about the account ",
+        type: swaggerParamsType.string,
+        required: true,
+      },
+      {
+        name: "userId",
+        in: swaggerParamsIn.path,
+        description: "This is required to map records to current userId ok ??",
+        type: swaggerParamsType.integer,
+      },
+      {
+        name: "soundId",
+        in: swaggerParamsIn.path,
+        description: "This is required to map records to current userId ok ??",
+        type: swaggerParamsType.integer,
+      },
+    ],
+  })
+  getLogin(
+    @getParam("id") id: string,
+    @getQuery("allGood")
+    q: string,
+    @Queries() query: any,
+    @Params() param: any
+  ) {
+    console.log("query", query);
+    console.log("params", param);
+
+    return { Hello: "world" };
+  }
+
+  @Get("/login")
+  login(
+    @getParam("id") id: string,
+    @getQuery("allGood") q: string,
+    @Queries() query: any
+  ) {
+    console.log("query", query);
+
+    return { Hello: "Login" };
+  }
+
+  @Post("/login/:id/:userId")
+  postLogin(
+    @Params() params: any,
+    @Queries() query: any,
+    @Body() body: any,
+    @getQuery("allGood") q: string,
+    @getBody("body1") bod1: string
+  ) {
+    return { hello: "post" };
+  }
 }
--- src/example/example.ts
+++ src/example/example.ts
@@ -1,15 +1,33 @@
-import express,{Request,Response} from 'express';
-import {Router} from 'express'
-import {InitializeDecorators} from '../'
-const app=express();
-const router=Router();
-app.get('/',(req:Request,res:Response)=>{ 
-    res.send({hello:"world"}) 
-})
- InitializeDecorators(router)
-app.use(router)
-app.listen('8080',()=>{
-    console.log("listening to port 8080 !");
-    
-})
-import  './SampleController'
+import express, { Request, Response } from "express";
+import { Router } from "express";
+import { InitializeDecorators, InitializeSwagger } from "../";
+
+const swaggerUi = require("swagger-ui-express"),
+  swaggerDocument = require("./swagger.json");
+
+const app = express();
+const router = Router();
+app.get("/", (req: Request, res: Response) => {
+  res.send({ hello: "world" });
+});
+InitializeDecorators(router);
+InitializeSwagger({
+  swagger: "2.0",
+  basePath: "/",
+  definitions: {},
+  host: "localhost:8080",
+  info: {
+    version: "1.0.0",
+    title: "Basic swagger implementation",
+    description: "A sample API here",
+  },
+  paths: {},
+  schemes: ["http", "https"],
+});
+app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(router);
+app.listen("8080", () => {
+  console.log("listening to port 8008 !");
+});
+import "./SampleController";
